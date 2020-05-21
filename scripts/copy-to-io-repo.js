afd4cfa89201f00d83d5de9c50448a6ddbfa5104
JaredMathis <jaredmathis@protonmail.com>
2020-05-21 06:48:14 -0400

    checkpoint

--- scripts/copy-to-io-repo.js
+++ scripts/copy-to-io-repo.js
@@ -2,42 +2,16 @@
 const fs = require('fs');
 var path = require('path');
 
-copyFolderRecursiveSync('./web', './../JaredMathis.github.io');
+var ncp = require('ncp').ncp;
+ 
+ncp.limit = 16;
+ 
+let source = './web';
+let destination = './../JaredMathis.github.io';
 
-// From: https://stackoverflow.com/a/26038979/569302
-function copyFileSync( source, target ) {
-
-    var targetFile = target;
-
-    //if target is a directory a new file with the same name will be created
-    if ( fs.existsSync( target ) ) {
-        if ( fs.lstatSync( target ).isDirectory() ) {
-            targetFile = path.join( target, path.basename( source ) );
-        }
-    }
-
-    fs.writeFileSync(targetFile, fs.readFileSync(source));
-}
-
-function copyFolderRecursiveSync( source, target ) {
-    var files = [];
-
-    //check if folder needs to be created or integrated
-    var targetFolder = path.join( target, path.basename( source ) );
-    if ( !fs.existsSync( targetFolder ) ) {
-        fs.mkdirSync( targetFolder );
-    }
-
-    //copy
-    if ( fs.lstatSync( source ).isDirectory() ) {
-        files = fs.readdirSync( source );
-        files.forEach( function ( file ) {
-            var curSource = path.join( source, file );
-            if ( fs.lstatSync( curSource ).isDirectory() ) {
-                copyFolderRecursiveSync( curSource, targetFolder );
-            } else {
-                copyFileSync( curSource, targetFolder );
-            }
-        } );
-    }
-}
+ncp(source, destination, function (err) {
+ if (err) {
+   return console.error(err);
+ }
+ console.log('done!');
+});
